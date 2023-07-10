b719b1ee339ab00b3468d6104f9b5da470316739
Jisna K M <jisnakm26@gmail.com>
2023-07-11 00:05:33 +0530

    validation added

--- routes/register.js
+++ routes/register.js
@@ -1,24 +1,44 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const User = require("../model/user.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../model/user.js");
 
 const router = express.Router();
 
 // Register a user
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { email, name, password, role } = req.body;
+  const docs = await User.findOne({ email: email });
 
-  const user = new User({ email, name, password, role });
+  if (docs == null) {
+    const user = new User({ email, name, password, role });
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).json({ status: "success", data:{user:{
-       email:user.email ,name:user.name, role:user.role, id:user.id}}});
-    })
-    .catch((error) => {
-      res.status(500).json({ status: "failed", data:{error:"Failed to register user" }});
+    user
+      .save()
+      .then(() => {
+        res.status(201).json({
+          status: "success",
+          data: {
+            user: {
+              email: user.email,
+              name: user.name,
+              role: user.role,
+              id: user.id,
+            },
+          },
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          status: "failed",
+          data: { error: "Failed to register user" },
+        });
+      });
+  } else {
+    res.status(500).json({
+      status: "failed",
+      data: { error: "this email alredy exists" },
     });
+  }
 });
 
 module.exports = router;
