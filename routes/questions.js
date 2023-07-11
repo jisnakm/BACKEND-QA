bfc9f9003bb84a55cd86e67b6f8ac2c51e42c195
Jisna K M <jisnakm26@gmail.com>
2023-07-11 16:46:13 +0530

    formatted the code

--- routes/questions.js
+++ routes/questions.js
@@ -5,66 +5,127 @@ const router = express.Router();
 
 // Create a Question schema and model
 const questionSchema = new mongoose.Schema({
-    category: { type: String, required: true},
-    title: { type: String, required: true },
-    answerType: { type: String, required: true },
-    answer: { type: String, required: true }
-  });
-  
-  const Question = mongoose.model('Question', questionSchema);
-  
-  
-  // Get all questions
-  router.get('/', (req, res) => {
-    Question.find()
-      .then((questions) => {
-        res.status(200).json(questions);
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Failed to fetch questions' });
-      });
-  });
-  
-  // Create a new question
-  router.post('/', (req, res) => {
-    const { category, title, answerType, answer } = req.body;
-  
-    const question = new Question({ category, title, answerType, answer });
-  
-    question.save()
-      .then(() => {
-        res.status(201).json({ message: 'Question created successfully' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Failed to create question' });
-      });
-  });
-  
-  // Update a question
-  router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { tcategory, title, answerType, answer } = req.body;
-  
-    Question.findByIdAndUpdate(id, { category, title, answerType, answer })
-      .then(() => {
-        res.status(200).json({ message: 'Question updated successfully' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Failed to update question' });
-      });
-  });
-  
-  // Delete a question
-  router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-  
-    Question.findByIdAndDelete(id)
-      .then(() => {
-        res.status(200).json({ message: 'Question deleted successfully' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Failed to delete question' });
-      });
-  });
+  category: { type: String, required: true },
+  title: { type: String, required: true },
+  answerType: { type: String, required: true },
+  answer: { type: String, required: true },
+});
 
-  module.exports = router;
+const Question = mongoose.model("Question", questionSchema);
+
+// Get all questions
+router.get("/", (req, res) => {
+  Question.find()
+    .then((questions) => {
+      allquestions = questions.map(
+        ({ id, category, title, answerType, answer }) => ({
+          id,
+          category,
+          title,
+          answerType,
+          answer,
+        })
+      );
+      res.status(200).json({
+        status: "success",
+        data: {
+          allquestions,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: "failed",
+        data: { error: "Failed to fetch questions" },
+      });
+    });
+});
+
+// Create a new question
+router.post("/", (req, res) => {
+  const { category, title, answerType, answer } = req.body;
+
+  const question = new Question({ category, title, answerType, answer });
+
+  question
+    .save()
+    .then(() => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          question: {
+            category: question.category,
+            title: question.title,
+            answerType: question.answerType,
+            answer: question.answer,
+            id: question.id,
+          },
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: "failed",
+        data: { error: "Failed to create question" },
+      });
+    });
+});
+
+// Update a question
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { category, title, answerType, answer } = req.body;
+
+  try {
+    const question = await Question.findByIdAndUpdate(
+    id,
+    { category, title, answerType, answer },
+    { new: true }
+  );
+  if (question !== null) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        question: {
+          category: question.category,
+          title: question.title,
+          answerType: question.answerType,
+          answer: question.answer,
+          id: question.id,
+        },
+      },
+    });
+  } else {
+    res.status(500).json({
+      status: "failed",
+      data: { error: "Failed to update question" },
+    });
+  }
+}
+catch(error){
+  res.status(500).json({
+    status: "failed",
+    data: { error: "internal server error" },
+  });
+}
+});
+
+// Delete a question
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Question.findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).json({
+        status: "success",
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: "failed",
+        data: { error: "Failed to delete question" },
+      });
+    });
+});
+
+module.exports = router;
